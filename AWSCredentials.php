c06b6742ea84f2f5b9901afa194c3e4f99d3db50
Randy Hommel <rhommel@ccctechcenter.org>
2016-12-18 12:29:03 -0800

    Modified the getSDKCredentialsArray to return either static keys or temp keys in the case of assumed role profiles

--- AWSCredentials.php
+++ AWSCredentials.php
@@ -297,59 +297,35 @@ class AWSCredentials
         return 0;
     }
 
-    function getSDKCredentialsArray() {
-        $credentials = array();
-        foreach ($this->profiles as $profilename => $data) {
-            if (isset($data['credentials']) && ($profilename != 'default')) {
-                $credentials[$profilename] = array(
-                    'key' => $data['credentials']['aws_access_key_id'],
-                    'secret' => $data['credentials']['aws_secret_access_key'],
-                );
+    function getSDKCredentialsArray($useprofile=NULL) {
+        $credentials = [
+            'key' => "",
+            'secret' => "",
+        ];
+        if (isset($this->profiles[$this->currentDefaultProfile])) $useprofile=$this->currentDefaultProfile;
+        if (($useprofile !== NULL) && (isset($this->profiles[$useprofile]))) {
+            if ( isset($this->profiles[$useprofile]['credentials']))  {
+                $credentials = [
+                    'key' => $this->profiles[$useprofile]['credentials']['aws_access_key_id'],
+                    'secret' => $this->profiles[$useprofile]['credentials']['aws_secret_access_key'],
+                    ];
+            } else {
+                $this->validAssumedRoleCredentials($useprofile);
+                if (isset($this->profiles[$useprofile]['tempcreds'])) {
+                    $credentials = [
+                        'key' => $this->profiles[$useprofile]['tempcreds']['AccessKeyId'],
+                        'secret' => $this->profiles[$useprofile]['tempcreds']['SecretAccessKey'],
+                        'token' => $this->profiles[$useprofile]['tempcreds']['SessionToken'],
+                    ];
+                }
             }
-        }
-        if (isset($credentials[$this->currentDefaultProfile])) {
-            $credentials['@default']=$this->currentDefaultProfile;
         } else {
-            $keys=array_keys($credentials);
-            $credentials['@default']=$keys[0];
+            if ((isset($this->profiles[$this->currentDefaultProfile])) && (isset($this->profiles[$this->currentDefaultProfile][''])) ) {
+
+            }
+            // do nothing yet...
         }
         return $credentials;
-    /*
-        array(
-
-            // Credentials for the development environment.
-            'development' => array(
-
-                // Amazon Web Services Key. Found in the AWS Security Credentials. You can also pass
-                // this value as the first parameter to a service constructor.
-                'key' => 'development-key',
-
-                // Amazon Web Services Secret Key. Found in the AWS Security Credentials. You can also
-                // pass this value as the second parameter to a service constructor.
-                'secret' => 'development-secret',
-
-                // This option allows you to configure a preferred storage type to use for caching by
-                // default. This can be changed later using the set_cache_config() method.
-                //
-                // Valid values are: `apc`, `xcache`, or a file system path such as `./cache` or
-                // `/tmp/cache/`.
-                'default_cache_config' => '',
-
-                // Determines which Cerificate Authority file to use.
-                //
-                // A value of boolean `false` will use the Certificate Authority file available on the
-                // system. A value of boolean `true` will use the Certificate Authority provided by the
-                // SDK. Passing a file system path to a Certificate Authority file (chmodded to `0755`)
-                // will use that.
-                //
-                // Leave this set to `false` if you're not sure.
-                'certificate_authority' => false
-            ),
-
-            // Specify a default credential set to use if there are more than one.
-            '@default' => 'development'
-        )
-            */
     }
 }
 
